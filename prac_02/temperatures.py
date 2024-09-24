fb9ad517a60ee6c4d5e0658f6f99baad00cf1721
benbowden <benjamin.bowden@my.jcu.edu.au>
2024-09-24 12:03:20 +1000

    Refactor celsius/ fahrenheit conversions to their own function outside the main the function

--- prac_02/temperatures.py
+++ prac_02/temperatures.py
@@ -7,18 +7,34 @@ MENU = """C - Convert Celsius to Fahrenheit
 F - Convert Fahrenheit to Celsius
 Q - Quit"""
 print(MENU)
-choice = input(">>> ").upper() # whether lower or upper case is entered, input is converted to upper case
-while choice != "Q": # While "Q" isn't selected, therefore "Q" ends the while loop
-    if choice == "C": # Select to enter a temperature in Celsius
-        celsius = float(input("Celsius: ")) # Celsius variable holds that input as a float
-        fahrenheit = celsius * 9.0 / 5 + 32 # Fahrenheit variable is derived from the celsius variable
-        print(f"Result: {fahrenheit:.2f} F") # Print the resulting fahrenheit variable
-    elif choice == "F": # Select to enter a temperature in Fahrenheit
-        fahrenheit = float(input("Fahrenheit: ")) # Fahrenheit variable holds that input as a float
-        celsius = (fahrenheit - 32) * 5/ 9.0 # Celsius variable is derived from the fahrenheit variable
-        print(f"Result: {celsius:.2f} C") # Print the resulting Celsius variable
-    else:
-        print("Invalid option") # Any other entry will be rejected as invalid
-    print(MENU)
-    choice = input(">>> ").upper()
-print("Thank you.")
+
+
+
+def main():
+    choice = input(">>> ").upper() # whether lower or upper case is entered, input is converted to upper case
+    while choice != "Q": # While "Q" isn't selected, therefore "Q" ends the while loop
+        if choice == "C": # Select to enter a temperature in Celsius
+            fahrenheit = celsius_to_fahrenheit()
+            print(f"Result: {fahrenheit:.2f} F") # Print the resulting fahrenheit variable
+        elif choice == "F": # Select to enter a temperature in Fahrenheit
+            celsius = fahrenheit_to_celsius()
+            print(f"Result: {celsius:.2f} C") # Print the resulting Celsius variable
+        else:
+            print("Invalid option") # Any other entry will be rejected as invalid
+        print(MENU)
+        choice = input(">>> ").upper()
+    print("Thank you.")
+
+    main()
+
+
+def fahrenheit_to_celsius():
+    fahrenheit = float(input("Fahrenheit: "))  # Fahrenheit variable holds that input as a float
+    celsius = (fahrenheit - 32) * 5 / 9.0  # Celsius variable is derived from the fahrenheit variable
+    return celsius
+
+
+def celsius_to_fahrenheit():
+    celsius = float(input("Celsius: "))  # Celsius variable holds that input as a float
+    fahrenheit = celsius * 9.0 / 5 + 32  # Fahrenheit variable is derived from the celsius variable
+    return fahrenheit
