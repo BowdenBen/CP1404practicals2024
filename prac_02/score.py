a5ab2bf2e4d2d340addcc37e4c14b8f003332104
benbowden <benjamin.bowden@my.jcu.edu.au>
2024-09-24 12:26:58 +1000

    Rewrite score_check to work as intended

--- prac_02/score.py
+++ prac_02/score.py
@@ -5,19 +5,19 @@ Broken program to determine score status
 
 def main():
     score = float(input("Enter score: ")) # Score variable holds input as a float
-    score_check(score)
-    print("Invalid Score")
+    result = score_check(score)
+    print(result)
 
 
 def score_check(score):
     while 0 <= score <= 100:  # while score is between 0-100 inclusive, loop continues to run
         if 90 <= score < 100:  # if, elif, else sets print conditions tied to variable score
-            print("Excellent")
+            return "Excellent"
         elif 50 <= score < 90:
-            print("Passable")
+            return "Passable"
         else:
-            print("Poor")
-        score = float(input("Enter score: "))  # loop continues for next user input
+            return "Poor"
+    print("Invalid Score")
 
 
 main()
