f1b5ec2fe863aa2d0e9bd2872e44c67fe8a18dc9
benbowden <benjamin.bowden@my.jcu.edu.au>
2024-09-24 12:44:24 +1000

    Add standard menu pattern

---
+++ prac_02/score_menu.py
@@ -0,0 +1,23 @@
+
+
+
+MENU = "(G)et a valid score (must be 0-100 inclusive)\n(P)rint result\n(S)how stars\n(Q)uit"
+
+print(MENU)
+
+
+display menu
+get choice
+while choice != <quit option>
+    if choice == <first option>
+        <do first task>
+    else if choice == <second option>
+        <do second task>
+    ...
+    else if choice == <n-th option>
+        <do n-th task>
+    else
+        display invalid input error message
+    display menu
+    get choice
+<do final thing, if needed>
