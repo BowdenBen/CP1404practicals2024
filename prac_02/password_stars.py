26427fe3c45b3e0c6bb363a3ff191ef9fbd23102
benbowden <benjamin.bowden@my.jcu.edu.au>
2024-09-24 11:55:06 +1000

    Refactor password check program to use functions

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -1,9 +1,23 @@
-min_length = 8
 
-password = input("Enter a password: ")
 
-while len(password) < min_length:
-    print("Password must be at lease {} characters long.".format(min_length))
+def main():
+    min_length = 8
+
+    password = check_password(min_length)
+
+    print_asterix(password)
+
+
+def print_asterix(password):
+    print(" * " * len(password))
+
+
+def check_password(min_length):
     password = input("Enter a password: ")
+    while len(password) < min_length:
+        print("Password must be at lease {} characters long.".format(min_length))
+        password = input("Enter a password: ")
+    return password
 
-print(" * " * len(password))
+
+main()
