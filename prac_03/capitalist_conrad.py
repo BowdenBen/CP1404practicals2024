dd89be13979af89f94e2b3ff54ba456fdddd5062
benbowden <benjamin.bowden@my.jcu.edu.au>
2024-10-02 12:32:15 +1000

    Update program so that it writes the output to a file. Generates file stock_price.txt

--- prac_03/capitalist_conrad.py
+++ prac_03/capitalist_conrad.py
@@ -14,9 +14,12 @@ MAX_DECREASE = 0.05  # 5%
 MIN_PRICE = 1
 MAX_PRICE = 100.0
 INITIAL_PRICE = 10.0
+FILENAME = "stock_price.txt"  # Define the file name
 
 price = INITIAL_PRICE
 number_of_days = 0  # Initialize the day counter
+
+out_file = open(FILENAME, 'w')
 
 print(f"${price:,.2f}")
 
@@ -35,4 +38,6 @@ while MIN_PRICE <= price <= MAX_PRICE:
         price_change = random.uniform(-MAX_DECREASE, 0)
 
     price *= (1 + price_change)
-    print(f"On day {number_of_days} price is: ${price:,.2f}")
+    print(f"On day {number_of_days} price is: ${price:,.2f}", file=out_file)
+
+out_file.close()
