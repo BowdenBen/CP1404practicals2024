b4c3a5e06206b2b34a0ba693190e878ce24d9344
benbowden <benjamin.bowden@my.jcu.edu.au>
2024-10-02 13:57:36 +1000

    Update code to make an exception for ValueError and finish loop should a valid integer be entered

--- prac_03/exceptions_to_complete.py
+++ prac_03/exceptions_to_complete.py
@@ -3,11 +3,13 @@ CP1404/CP5632 - Practical
 Fill in the TODOs to complete the task
 """
 
-is_finished = False
-while not is_finished:
+is_finished = False # Use boolean to set variable so loop can be entered
+while not is_finished: # while loop based on boolean
     try:
+        # Attempt to get a valid integer from the user
         result = int(input("Enter a valid integer: "))
-        # TODO: this line
-    except:  # TODO - add the exception you want to catch after except
+        is_finished = True # this finishes the loop if an integer is entered
+    except ValueError: # ValueError catches anything tha is not an integer
         print("Please enter a valid integer.")
+# Output the valid integer once it has been successfully entered
 print("Valid result is:", result)
