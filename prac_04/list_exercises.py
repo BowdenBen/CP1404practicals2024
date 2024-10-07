b31336c6de042418b4e5f4b63a4347c68e19bf63
benbowden <benjamin.bowden@my.jcu.edu.au>
2024-10-07 11:06:01 +1000

    Create list_exercises.py, write psuedocode for program

---
+++ prac_04/list_exercises.py
@@ -0,0 +1,19 @@
+'''
+START
+
+    CREATE an empty list called numbers
+
+    FOR each number from 1 to 5:
+        PROMPT the user to input a number
+        CONVERT the input to a floating-point number
+        ADD the number to the numbers list
+
+    DISPLAY "The first number is" followed by the first element of the numbers list
+    DISPLAY "The last number is" followed by the last element of the numbers list
+    DISPLAY "The smallest number is" followed by the smallest element in the numbers list
+    DISPLAY "The largest number is" followed by the largest element in the numbers list
+    CALCULATE the average by dividing the sum of the numbers list by the number of elements in the list
+    DISPLAY "The average of the numbers is" followed by the calculated average
+
+END
+'''
