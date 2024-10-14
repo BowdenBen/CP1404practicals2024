b680ee312fa664eb3436fa9338a4dde8a95fffe2
benbowden <benjamin.bowden@my.jcu.edu.au>
2024-10-14 13:30:45 +1000

    Create emails.py, write header comment with time estimation.

---
+++ prac_05/emails.py
@@ -0,0 +1,8 @@
+"""
+Write a program that stores users' emails (unique keys) and names (values) in a dictionary.
+Ask the user for their email until they enter a blank one.
+By Benjamin Bowden
+Estimate: 30 minutes
+Actual:    minutes
+"""
+
