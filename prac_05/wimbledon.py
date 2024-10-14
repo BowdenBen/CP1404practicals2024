1f1950a7bb2a9260552886325a0f33c5cad4cf60
benbowden <benjamin.bowden@my.jcu.edu.au>
2024-10-14 15:55:53 +1000

    Develop function to process the information from the .csv file

--- prac_05/wimbledon.py
+++ prac_05/wimbledon.py
@@ -9,17 +9,32 @@ Actual:   minutes
 
 filename = "wimbledon.csv"
 
-FUNCTION process_file(file):
-OPEN file "wimbledon_data.csv" with encoding "utf-8-sig"
-    champions_list, champions_dict, countries_set = process_file(file)
-    INIT empty list matches, dictionary champions_dict, set countries_set
-    SKIP header line
-    FOR each line in file:
-        SPLIT line by commas
-        ADD match data to matches
-        INCREMENT champion's count in champions_dict
-        ADD champion's country to countries_set
-    RETURN matches, champions_dict, countries_set
+def process_file(filename):
+    """Open the data file, extract and process information"""
+    # Initialise dictionary and list needed
+    champions = []
+    champions_dict = {}
+    champion_countries = set() # creates an empty set in Python
+    with open(filename, "r", encoding="utf-8-sig") as file: # open file using utf 8 encoding
+        next(file) # skip the first line of the file
+        for line in file:
+            data = line.strip().split(",") # strip removes leading and lagging space, split us comma as delimiter
+
+            # Unpack the data into meaningful variables
+            year, champion_country, champion, runner_up_country, runner_up, score = data
+
+            # Append to the list of champions
+            champions.append([year, champion_country, champion, runner_up_country, runner_up, score])
+
+            # Update the champions dictionary (count the number of wins per champion)
+            if champion in champions_dict:
+                champions_dict[champion] += 1
+            else:
+                champions_dict[champion] = 1
+
+            champion_countries.add(champion_country)
+
+    return champions, champions_dict, champion_countries
 
 FUNCTION display_champions(champions_dict):
     FOR each champion, wins in champions_dict:
@@ -29,7 +44,7 @@ FUNCTION display_countries(countries_set):
     SORT countries_set
     PRINT ', '.join(countries_set)
 
-FUNCTION main():
+def main():
     PRINT "Wimbledon Champions:"
     CALL display_champions(champions_dict)
     PRINT "These countries have won Wimbledon:"
