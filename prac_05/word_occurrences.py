5a63d3239e4fb10e5d7603bbb270397cd27ab196
benbowden <benjamin.bowden@my.jcu.edu.au>
2024-10-14 13:04:47 +1000

    oop through list of words and add count as a dictionary

--- prac_05/word_occurrences.py
+++ prac_05/word_occurrences.py
@@ -5,8 +5,6 @@ Estimate: 20 minutes
 Actual:   32 minutes
 """
 
-# Function to count occurrences of words in a string
-count_word_occurrences():
 
 # Prompt user for a string input
 input_string = input("Enter a string: ")
@@ -18,15 +16,16 @@ words = input_string.split()
 word_count = {}
 
 # Loop through each word in the list of words
-for each word in words:
-
+for word in words:
     # If the word is already in the dictionary
-    if word exists in word_count:
+    if word in word_count:
         # Increment the word's count
         word_count[word] += 1
     else:
         # Otherwise, add the word to the dictionary with a count of 1
         word_count[word] = 1
+
+print(word_count)
 
 # Find the length of the longest word for formatting
 max_length = find the length of the longest word in word_count.keys()
