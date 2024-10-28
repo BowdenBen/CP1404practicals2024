1a941bce42e81c85b89baa40b9044d5b26ee2276
benbowden <benjamin.bowden@my.jcu.edu.au>
2024-10-28 13:53:44 +1000

    Complete psuedocode for languages.py defining structure pf the program.

---
+++ prac_06/languages.py
@@ -0,0 +1,13 @@
+# Import ProgrammingLanguage
+
+# Create language objects
+
+# Print the string representation of Python
+
+# Create a list of languages
+# languages = [python, ruby, visual_basic]
+
+# Print "The dynamically typed languages are:"
+
+# Loop through the languages list, to access information
+
