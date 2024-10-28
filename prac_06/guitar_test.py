72be5f565026cc18a9319a63602978d5905ba7f2
benbowden <benjamin.bowden@my.jcu.edu.au>
2024-10-28 15:05:40 +1000

    Create guitar_test.py, write psuedocode for functionality

---
+++ prac_06/guitar_test.py
@@ -0,0 +1,13 @@
+# Import Guitar
+#
+# # Create two Guitar objects
+# guitar1 = Guitar("Gibson L-5 CES", 1922, 16035.40)
+# guitar2 = Guitar("Another Guitar", 2013, 0)
+#
+# # Test get_age() method
+# Print "Gibson L-5 CES get_age() - Expected 100. Got", guitar1.get_age()
+# Print "Another Guitar get_age() - Expected 9. Got", guitar2.get_age()
+#
+# # Test is_vintage() method
+# Print "Gibson L-5 CES is_vintage() - Expected True. Got", guitar1.is_vintage()
+# Print "Another Guitar is_vintage() - Expected False. Got", guitar2.is_vintage()
