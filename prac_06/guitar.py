46a13ceb49dc702866dc494691d09dbf6b20e9b7
benbowden <benjamin.bowden@my.jcu.edu.au>
2024-10-28 14:52:34 +1000

    Write psuedocode for a guitar class, methods defined as per prac 06.

---
+++ prac_06/guitar.py
@@ -0,0 +1,19 @@
+# Class Guitar:
+#
+#     Method __init__(name="", year=0, cost=0):
+#         Set self.name to name
+#         Set self.year to year
+#         Set self.cost to cost
+#
+#     Method __str__():
+#         Return formatted string of guitar details: name, year, cost
+#
+#     Method get_age():
+#         Calculate age by subtracting self.year from current year
+#         Return age
+#
+#     Method is_vintage():
+#         If get_age() is greater than or equal to 50:
+#             Return True
+#         Else:
+#             Return False
