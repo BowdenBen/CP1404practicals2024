4c55d5d639d58042ea81dfc5b4899cf3f9693894
benbowden <benjamin.bowden@my.jcu.edu.au>
2024-10-28 13:51:09 +1000

    Complete psuedocode for 3 methods to be used in programming_language.py, based on instruction given in Prac 06.

--- prac_06/programming_language.py
+++ prac_06/programming_language.py
@@ -1,9 +1,16 @@
-# `Class ProgrammingLanguage:
+# Class ProgrammingLanguage:
 #
 #     Method __init__(name, typing, reflection, year):
-
+#         Set self.name to name
+#         Set self.typing to typing
+#         Set self.reflection to reflection
+#         Set self.year to year
 #
 #     Method is_dynamic():
-
+#         If self.typing equals "Dynamic":
+#             Return True
+#         Else:
+#             Return False
 #
 #     Method __str__():
+#         Return formatted string with name, typing, reflection, and year
