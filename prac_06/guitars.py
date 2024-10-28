bb431a016e2a75fdaf87913b385e44e7632edafa
benbowden <benjamin.bowden@my.jcu.edu.au>
2024-10-28 15:17:16 +1000

    Create guitars.py, write basic psuedocode for program to follow.

---
+++ prac_06/guitars.py
@@ -0,0 +1,17 @@
+"""
+Time to completion EST: 30 hour
+Actual Time to Completion:
+"""
+
+
+# Import Guitar
+
+# Create an empty list to store guitars
+# guitars = []
+
+# Print "My guitars!"
+
+# Input loop to get guitar details
+
+
+ # Print all stored guitars
