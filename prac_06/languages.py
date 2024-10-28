4e8ada251e94e492b1e3deb16ec4dbec51d53d80
benbowden <benjamin.bowden@my.jcu.edu.au>
2024-10-28 14:06:37 +1000

    Define psuedocode of languages.py as per prac 06 instructions

--- prac_06/languages.py
+++ prac_06/languages.py
@@ -1,13 +1,19 @@
 # Import ProgrammingLanguage
-
-# Create language objects
-
-# Print the string representation of Python
-
-# Create a list of languages
+#
+# # Create language objects
+# python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
+# ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
+# visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
+#
+# # Print the string representation of Python
+# Print python
+#
+# # Create a list of languages
 # languages = [python, ruby, visual_basic]
-
+#
 # Print "The dynamically typed languages are:"
-
-# Loop through the languages list, to access information
-
+#
+# # Loop through the languages list
+# For language in languages:
+#     If language.is_dynamic():
+#         Print language.name
