f9e35423c1acbf7ba7690299492a736920cd31e5
benbowden <benjamin.bowden@my.jcu.edu.au>
2024-10-28 15:59:43 +1000

    Write code for printing guitars list

--- prac_06/guitars.py
+++ prac_06/guitars.py
@@ -25,13 +25,12 @@ def main():
 
         name = input("Name: ")
 
-    # Print
-    # "These are my guitars:"
-    #
-    # For i, guitar in enumerate(guitars, 1):
-    # vintage_string = " (vintage)" if guitar.is_vintage() else ""
-    # Print
-    # f"Guitar {i}: {guitar.name:>20} ({guitar.year}), worth ${guitar.cost:10,.2f}{vintage_string}"
+    # Print all stored guitars
+    print("\nThese are my guitars:")
+    for i, guitar in enumerate(guitars, 1):
+        vintage_string = " (vintage)" if guitar.is_vintage() else ""
+        print(f"Guitar {i}: {guitar.name:>20} ({guitar.year}), worth ${guitar.cost:10,.2f}{vintage_string}")
+
 
 if __name__ =="__main__":
     main()
