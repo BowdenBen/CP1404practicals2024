ea1332471b223d3d0044f3f2053baf4ab77e222e
benbowden <benjamin.bowden@my.jcu.edu.au>
2024-10-28 15:13:41 +1000

    test is_vintage method of class. Update time taken to complete guitar.py

--- prac_06/guitar.py
+++ prac_06/guitar.py
@@ -1,6 +1,6 @@
 """
 Time to completion EST: 1 hour
-Actual Time to Completion:
+Actual Time to Completion: 30 min
 """
 
 class Guitar:
--- prac_06/guitar_test.py
+++ prac_06/guitar_test.py
@@ -10,6 +10,6 @@ guitar2 = Guitar("Another Guitar", 2013, 250.00)
 print(f"Gibson L-5 CES get_age() - Expected 102. Got", guitar1.get_age())
 print(f"Another Guitar get_age() - Expected 11. Got", guitar2.get_age())
 
-# # Test is_vintage() method
-# Print "Gibson L-5 CES is_vintage() - Expected True. Got", guitar1.is_vintage()
-# Print "Another Guitar is_vintage() - Expected False. Got", guitar2.is_vintage()
+# Test is_vintage() method
+print(f"Gibson L-5 CES is_vintage() - Expected True. Got", guitar1.is_vintage())
+print(f"Another Guitar is_vintage() - Expected False. Got", guitar2.is_vintage())
