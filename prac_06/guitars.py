766454074774802708baf4ac21248edbff9ad0a9
benbowden <benjamin.bowden@my.jcu.edu.au>
2024-10-28 15:46:47 +1000

    Write psuedocode for printing guitars list

--- prac_06/guitars.py
+++ prac_06/guitars.py
@@ -25,8 +25,13 @@ def main():
 
         name = input("Name: ")
 
-
- # Print all stored guitars
+    # Print
+    # "These are my guitars:"
+    #
+    # For i, guitar in enumerate(guitars, 1):
+    # vintage_string = " (vintage)" if guitar.is_vintage() else ""
+    # Print
+    # f"Guitar {i}: {guitar.name:>20} ({guitar.year}), worth ${guitar.cost:10,.2f}{vintage_string}"
 
 if __name__ =="__main__":
     main()
