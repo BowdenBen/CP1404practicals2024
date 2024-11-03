ed51893a36fb4d7f784b250cdfcad75d4007f039
benbowden <benjamin.bowden@my.jcu.edu.au>
2024-11-03 16:29:50 +1000

    define psuedocode for class with more detail

--- prac_07/project.py
+++ prac_07/project.py
@@ -5,7 +5,6 @@ Actual Time to Completion:
 
 class Project
 
-
 # Define class Project:
 #     Attributes:
 #         name - string for project name
@@ -13,4 +12,23 @@ class Project
 #         priority - integer for priority level
 #         cost - float for cost estimate
 #         completion - integer for completion percentage
+#
+#     Define __init__ method with parameters (name, start_date, priority, cost, completion):
+#         Initialize attributes with parameter values
+#
+#     Define __str__ method:
+#         Return formatted string with project details
+#
+#     Define is_complete method:
+#         Return True if completion is 100, otherwise False
+#
+#     Define update_completion method with parameter new_completion:
+#         Set completion to new_completion
+#
+#     Define update_priority method with parameter new_priority:
+#         Set priority to new_priority
+#
+#     Define __lt__ method with parameter other:
+#         Return True if this project’s priority is less than other’s priority
+
 
