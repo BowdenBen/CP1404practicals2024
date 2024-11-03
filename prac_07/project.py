8670a3fea3a53006b366f30c922ba3cf776b6595
benbowden <benjamin.bowden@my.jcu.edu.au>
2024-11-03 16:42:58 +1000

    Comment for clarity

--- prac_07/project.py
+++ prac_07/project.py
@@ -6,32 +6,33 @@ Actual Time to Completion:
 import datetime
 
 class Project:
-    """Initialize attributes with parameter values"""
     def __init__(self, name, start_date, priority, cost, completion):
+        """Initialize a new Project instance with the given attributes."""
         self.name = name
-        self.start_date = start_date  # Should be a date object
+        self.start_date = start_date  # This should be a date object
         self.priority = priority
         self.cost = cost
         self.completion = completion
 
-
     def __str__(self):
+        """Return a string representation of the Project object."""
         return f"{self.name}, start: {self.start_date}, priority: {self.priority}, cost: ${self.cost:.2f}, completion: {self.completion}%"
 
-
     def is_complete(self):
+        """Return True if the project is 100% complete."""
         return self.completion == 100
 
-
     def update_completion(self, new_completion):
+        """Update the completion percentage of the project."""
         self.completion = new_completion
 
-
     def update_priority(self, new_priority):
+        """Update the priority of the project."""
         self.priority = new_priority
 
-
     def __lt__(self, other):
+        """Compare projects based on priority for sorting."""
         return self.priority < other.priority
+
 
 
