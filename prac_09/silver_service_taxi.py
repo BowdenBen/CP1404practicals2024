49ed748f8e6b066311603303d6e1100d4b1673c6
benbowden <benjamin.bowden@my.jcu.edu.au>
2024-11-20 13:14:42 +1000

    Define the class arguments, use kwargs to bring in parent arguments. Add a class variable.

--- prac_09/silver_service_taxi.py
+++ prac_09/silver_service_taxi.py
@@ -2,7 +2,13 @@
 from taxi import Taxi
 
 class SilverServiceTaxi(Taxi):
+    """A special Taxi subclass with additional fanciness."""
+
+    flagfall = 4.50  # Class-level constant for SilverServiceTaxi flagfall
 
     def __init__(self, fanciness, **kwargs):
+        """Initialize a SilverServiceTaxi with fanciness and other Taxi attributes."""
         super().__init__(self, **kwargs)
         self.fanciness = fanciness
+        self.price_per_km = Taxi.price_per_km * fanciness #fanciness scales the Taxi classes prices per km.
+
