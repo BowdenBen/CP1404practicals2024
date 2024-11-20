bf8c5639cc3aaa13f9c088d08f16de42db24feee
benbowden <benjamin.bowden@my.jcu.edu.au>
2024-11-20 12:31:55 +1000

    Set Taxi class variable price_per_km, test.

--- prac_09/taxi_test.py
+++ prac_09/taxi_test.py
@@ -1,7 +1,7 @@
 from taxi import Taxi
 
 def main():
-    p1 = Taxi(name='Prius 1', fuel=100, price_per_km=1.23)
+    p1 = Taxi(name='Prius 1', fuel=100)
     # Drive the car for 40 km
     p1.drive(40)
     print(p1)
