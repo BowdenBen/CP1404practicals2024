12149d9384eb89c42f960930892871df5052ded6
benbowden <benjamin.bowden@my.jcu.edu.au>
2024-11-22 13:01:38 +1000

    defined play method and string method

--- prac_09/band.py
+++ prac_09/band.py
@@ -11,4 +11,18 @@ class Band:
 
     def add(self, musician):
         """Add a musician to the band."""
-            self.musicians.append(musician)  # Add the musician to the list
+        self.musicians.append(musician)  # Add the musician to the list
+
+    def play(self):
+        """Play the band."""
+        performances = []
+        for musician in self.musicians:
+            if musician in musician.instruments: # Check if musician has an instrument
+                performances.append(musician.play())
+            else:
+                performances.append(f"{musician.name} needs ands instrument.")
+        return "\n".join(performances)
+
+    def __str__(self):
+        """Return a string representation of the band."""
+        return f"{self.name} ({', '.join(str(musician) for musician in self.musicians)})"
