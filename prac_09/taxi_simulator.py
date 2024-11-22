d84521f867531e5b6c4b001b40d05e46807b84a0
benbowden <benjamin.bowden@my.jcu.edu.au>
2024-11-22 14:12:34 +1000

    Set starting values and lists.

--- prac_09/taxi_simulator.py
+++ prac_09/taxi_simulator.py
@@ -1,14 +1,20 @@
 from taxi import Taxi
 from silver_service_taxi import SilverServiceTaxi
 
-"""
-Initialise Taxis
-    Create list of taxis
-    set current (starting taxi to none)
-    set starting bill to $0
-    
-Print welcome message
 
+def main():
+    """Taxi Simulator Program"""
+    taxis = [Taxi(name="Prius", fuel=100), SilverServiceTaxi(name="Limo",fuel=100, fanciness=2),
+             SilverServiceTaxi(name="Hummer", fuel=200, fanciness=4)]
+    current_taxi = None
+    total_bill = 0.0
+    choice = ""
+
+    menu = "q)uit, c)hoose, d)rive"
+    print("Let's Drive!")
+
+
+"""
 While loop set to "doesn't equal q"
     print menu options
     get menu choice
