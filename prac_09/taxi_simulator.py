ad75fa128096224b334514b2acaf971bde8276ac
benbowden <benjamin.bowden@my.jcu.edu.au>
2024-11-22 14:58:15 +1000

    Used extract method to process menu selections outside of the main function.

--- prac_09/taxi_simulator.py
+++ prac_09/taxi_simulator.py
@@ -18,34 +18,43 @@ def main():
         choice = input(">>> ").lower()
 
         if choice =="c":
-            print("Taxis Available:")
-            for i, taxi in enumerate(taxis):
-                print(f"{i} - {taxi}")
-            try:
-                taxi_choice = int(input("Please choose a Taxi."))
-                current_taxi = taxis[taxi_choice]
-            except ValueError:
-                print("That's not a valid Taxi.")
-
+            current_taxi = taxi_selection(current_taxi, taxis)
         elif choice == "d":
-            if current_taxi is None:
-                print("You need to choose a valid taxi.")
-            else:
-                current_taxi.start_fare()
-                try:
-                    distance = int(input("Please choose how far to drive."))
-                    distance_driven = current_taxi.drive(distance)
-                    trip_cost = current_taxi.get_fare()
-                    total_bill += trip_cost
-                    print(f"Your {current_taxi.name} trip cost you ${trip_cost:.2f}")
-                except ValueError:
-                    print("Invalid distance")
+            total_bill = drive_selection(current_taxi, total_bill)
         elif choice != "q":
             print("Invalid choice.")
 
         print(f"Your bill to date: ${total_bill:.2f}")
 
     print(f"Total trip cost: ${total_bill:.2f}")
+
+
+def drive_selection(current_taxi, total_bill):
+    if current_taxi is None:
+        print("You need to choose a valid taxi.")
+    else:
+        current_taxi.start_fare()
+        try:
+            distance = int(input("Please choose how far to drive."))
+            distance_driven = current_taxi.drive(distance)
+            trip_cost = current_taxi.get_fare()
+            total_bill += trip_cost
+            print(f"Your {current_taxi.name} trip cost you ${trip_cost:.2f}")
+        except ValueError:
+            print("Invalid distance")
+    return total_bill
+
+
+def taxi_selection(current_taxi, taxis):
+    print("Taxis Available:")
+    for i, taxi in enumerate(taxis):
+        print(f"{i} - {taxi}")
+    try:
+        taxi_choice = int(input("Please choose a Taxi."))
+        current_taxi = taxis[taxi_choice]
+    except ValueError:
+        print("That's not a valid Taxi.")
+    return current_taxi
 
 
 if __name__ == "__main__":
