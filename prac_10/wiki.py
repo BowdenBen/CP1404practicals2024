5e708988d0b5088f8edf6b3eb821e5516c1e11b6
benbowden <benjamin.bowden@my.jcu.edu.au>
2024-12-01 12:29:01 +1000

    Add try/except to handle a PageError.

--- prac_10/wiki.py
+++ prac_10/wiki.py
@@ -1,13 +1,19 @@
 import wikipedia
+from wikipedia import PageError
+
 
 def main():
     print("Welcome to the Wikipedia Search Engine.")
     title = input("Please enter a title or search phrase.\n>>>  ")
     while title != "":
-        page = wikipedia.page(title, auto_suggest=False)
-        print(f"\n{page.title}\n")
-        print(page.url)
-        print(f"\n{page.summary[:1000]}...")
+        try:
+            page = wikipedia.page(title, auto_suggest=False)
+            print(f"\n{page.title}\n")
+            print(page.url)
+            print(f"\n{page.summary[:1000]}...")
+        except PageError:
+            # Handle page not found error
+            print(f"\nPage id \"{title}\" does not match any pages. Try another id!")
         title = input("Please enter a title or search phrase.\n>>>  ")
     print("Thank you for using this program.")
 
